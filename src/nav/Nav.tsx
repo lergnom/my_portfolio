d86b13ffa06d9b8bba0fdd6fd6d0c702f1e65db7
lergnom <lergnom@mail.ru>
2021-07-27 16:57:05 +0300

    added nav

---
+++ src/nav/Nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./Nav.module.css"
+
+function Header() {
+
+    return (
+        <div className = {style.header}>
+        </div>
+    )
+
+}
+
+export default Header
