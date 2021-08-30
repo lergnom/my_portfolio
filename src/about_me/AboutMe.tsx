885a27ff0c0b43e01fd572faf10374739afd52ab
lergnom <lergnom@mail.ru>
2021-08-30 16:59:47 +0300

    section about me

--- src/about_me/AboutMe.tsx
+++ src/about_me/AboutMe.tsx
@@ -1,22 +1,57 @@
 import React from "react";
-import style from "./AboutMe.module.css"
+import style from "./AboutMe.module.scss"
 import styleContainer from './../common/styles/Container.module.css'
+
 
 const AboutMe = () => {
 
     return (
-        <div className={style.aboutMe}>
+        // <div className={style.aboutMe}>
+        //     <div className={`${styleContainer.container} ${style.container}`}>
+        //         <div className={style.info}>
+        //             <span>Привет!</span>
+        //             <h1>Меня зовут Асмоловский Антон</h1>
+        //             <p>Я человек с планеты Земля</p>
+        //         </div>
+        //         <div className={style.photo}>Photo</div>
+        //     </div>
+        // </div>
+
+        <section className={style.aboutMe}>
             <div className={`${styleContainer.container} ${style.container}`}>
                 <div className={style.info}>
-                    <span>Привет!</span>
-                    <h1>Меня зовут Асмоловский Антон</h1>
-                    <p>Я человек с планеты Земля</p>
+                    <div>
+                        <h1>Anton</h1>
+                        <h2>smolovskiy</h2>
+                    </div>
+
+                    <p className={style.intro}>I'm a lead programmer && service engeener, from Russia.</p>
+                    <p className={style.description}>At work, I program industrial controllers, make sure that everyone
+                        has a stable Internet and peripheral equipment does not fail.
+                        In my spare time, I do web development, play with children and travel on a motorcycle.</p>
                 </div>
-                <div className={style.photo}>Photo</div>
+                <div className={style.photo}>
+                    <img src="#"
+                         alt="anton asmolovskiy"/>
+                    <svg width="100px" height="100px" viewBox="0 0 42 42" className="donut">
+                        <circle className="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"> </circle>
+                        <circle className="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent"
+                                stroke="#d2d3d4" stroke-width="3"> </circle>
+                        <circle className="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent"
+                                stroke="#ce4b99" stroke-width="3" stroke-dasharray="85 15"
+                                stroke-dashoffset="25"> </circle>
+                        <circle className="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent"
+                                stroke="#b1c94e" stroke-width="3" stroke-dasharray="15 85"
+                                stroke-dashoffset="25"> </circle>
+                    </svg>
+
+                </div>
             </div>
-        </div>
+        </section>
     )
 
 }
 
 export default AboutMe
+
+
--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -7,13 +7,30 @@ const Nav = () => {
         <nav className={style.nav}>
             <ul>
                 <li>
-                    <a href="main">Главная</a>
+                    <a href="main">portfolio</a>
                 </li>
                 <li>
-                    <a href="main">Главная</a>
-                </li>  <li>
-                <a href="main">Главная</a>
-            </li>
+                    <a href="main">skills</a>
+                </li>
+                <li>
+                    <a href="main">contacts</a>
+                </li>
+                <li>
+                    <ul className={style.social}>
+                        <li className={style.twitter}><a href="https://www.twitter.com/AdhamDannaway"
+                                                         title="Follow me on Twitter"
+                                                         target="_blank">twitter</a></li>
+                        <li className={style.linkedin}><a href="https://au.linkedin.com/in/adhamdannaway"
+                                                          title="Connect with me on Linkedin"
+                                                          target="_blank">linkedin</a></li>
+                        <li className={style.facebook}><a href="https://www.facebook.com/ilikeadham"
+                                                          title="Like me on Facebook"
+                                                          target="_blank">facebook</a></li>
+                        <li className={style.instagram}><a href="https://www.instagram.com/adham.dannaway"
+                                                           title="Follow me on Instagram" target="_blank">instagram</a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
 
 
