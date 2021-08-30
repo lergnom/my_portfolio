eb32488951761f987d61b02bbb1b3a21d978ebe7
lergnom <lergnom@mail.ru>
2021-08-30 17:05:52 +0300

    svg Diagramm

--- src/about_me/AboutMe.tsx
+++ src/about_me/AboutMe.tsx
@@ -33,17 +33,7 @@ const AboutMe = () => {
                 <div className={style.photo}>
                     <img src="#"
                          alt="anton asmolovskiy"/>
-                    <svg width="100px" height="100px" viewBox="0 0 42 42" className="donut">
-                        <circle className="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"> </circle>
-                        <circle className="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent"
-                                stroke="#d2d3d4" stroke-width="3"> </circle>
-                        <circle className="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent"
-                                stroke="#ce4b99" stroke-width="3" stroke-dasharray="85 15"
-                                stroke-dashoffset="25"> </circle>
-                        <circle className="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent"
-                                stroke="#b1c94e" stroke-width="3" stroke-dasharray="15 85"
-                                stroke-dashoffset="25"> </circle>
-                    </svg>
+
 
                 </div>
             </div>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -10,7 +10,19 @@ const Skill = ({skillTitle, skillDesc}: SkillType) => {
 
     return (
         <div className={style.skill}>
-            <div className={style.skillIcon}></div>
+            <div className={style.skillIcon}>
+                <svg width="100%" height="100%" viewBox="0 0 42 42" className="donut">
+                    <circle className="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
+                    <circle className="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent"
+                            stroke="#d2d3d4" stroke-width="3"></circle>
+                    <circle className="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent"
+                            stroke="#ce4b99" stroke-width="3" stroke-dasharray="85 15"
+                            stroke-dashoffset="25"></circle>
+                    <circle className="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent"
+                            stroke="#b1c94e" stroke-width="3" stroke-dasharray="15 85"
+                            stroke-dashoffset="25"></circle>
+                </svg>
+            </div>
             <h3 className={style.skillTitle}>{skillTitle}</h3>
             <p className={style.skillDesc}>{skillDesc}</p>
         </div>
