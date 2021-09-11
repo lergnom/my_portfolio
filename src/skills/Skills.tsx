db502028dd07dd389df7d31c40caa97f3e40b5c8
lergnom <lergnom@mail.ru>
2021-09-11 12:55:16 +0300

    added skill

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -9,11 +9,12 @@ const Skills = () => {
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
-                <h2 className={style.title}>Опыт</h2>
+                <h2 className={style.title}>Skills</h2>
                 <div className={style.skills}>
                     <Skill skillTitle={'JS'} skillDesc={'TypeScript, UnitTests, Git'}/>
                     <Skill skillTitle={'React'} skillDesc={'HOC, HOOK, REDUX'}/>
                     <Skill skillTitle={'HTML'} skillDesc={'Разработка веб сайтов'}/>
+                    <Skill skillTitle={'PHP'} skillDesc={'MVC,RedBean, MySQL, PDO'}/>
                 </div>
             </div>
         </div>
