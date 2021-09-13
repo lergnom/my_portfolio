954c6d4c178aa8ee2f3a1b4a150a96d3b66224a5
lergnom <lergnom@mail.ru>
2021-09-13 09:17:09 +0300

    added forest travel

--- src/works/Works.tsx
+++ src/works/Works.tsx
@@ -3,6 +3,8 @@ import style from "./Works.module.scss"
 import styleContainer from './../common/styles/Container.module.css'
 import Work from "./work/Work";
 
+import forestTravel from './myworks/HTML/forestTravel.jpg'
+
 
 const Works = () => {
 
@@ -12,7 +14,7 @@ const Works = () => {
                 <h3>My works</h3>
                 <div className={style.works}>
                     <Work workTitle={"Социальная сеть"} workDesc={'Пример работы'}
-                          workImg={'itphotos_59728757-stock-photo-waiting-for-a-new-day.jpg'}
+                          workImg={forestTravel}
                           workLink={'#'}/>
                     <Work workTitle={"Список дел"}
                           workDesc={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur cupiditate eveniet harum provident? Ab\n' +
