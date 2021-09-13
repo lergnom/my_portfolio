3d3125259e784b57762270cec898b9a8f227e595
lergnom <lergnom@mail.ru>
2021-09-13 15:59:35 +0300

    work with footer

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -9,16 +9,22 @@ const Contacts = () => {
             <div className={`${styleContainer.container} ${style.container}`}>
                 <h3 className={style.title}>Contacts</h3>
                 <form className={style.form} action="">
-                    <label>Ваше имя:</label>
-                    <input className={style.input} type="text" name="fio" placeholder="Антон" value=""/>
-                    <label>Телефон:</label>
-                    <input className={style.input} type="text" name="tel" placeholder="9210707007" value=""/>
-                    <label>Почта:</label>
-                    <input className={style.input} type="text" name="mail" placeholder="mail@gmail.com" value=""/>
-                    <label>Сообщение:</label>
-                    <textarea className={style.input} name="description"></textarea>
-                    <button className={'toHireMe'} name="sendForm" value="formButton" type="submit">Отправить
-                    </button>
+                    <div className={style.leftSide}>
+                        <label>Name:</label>
+                        <input className={style.input} type="text" name="fio" placeholder="Anton"/>
+
+                        <label>Email address</label>
+                        <input className={style.input} type="text" name="mail" placeholder="lergnom@mail.ru"/>
+                    </div>
+                    <div>
+                        <label>Message</label>
+                        <textarea className={`${style.input} ${style.rightSide}`} name="description"></textarea>
+                        <button className={`toHireMe ${style.submit}`} name="sendForm" value="formButton"
+                                type="submit">Отправить
+                        </button>
+                    </div>
+
+
                 </form>
             </div>
 
--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./Footer.module.css"
+import style from "./Footer.module.scss"
 import container from "./../common/styles/Container.module.css"
 
 const Footer = () => {
@@ -7,14 +7,14 @@ const Footer = () => {
     return (
         <div className={style.footer}>
             <div className={`${container.container} ${style.container}`}>
-                <h3 className={style.title}>Антон Асмоловский</h3>
+                <h3 className={style.title}>Anton Asmolovskiy</h3>
                 <div className={style.social}>
                     <a  className={style.socialIcon} href="#"></a>
                     <a  className={style.socialIcon} href="#"></a>
                     <a  className={style.socialIcon} href="#"></a>
                     <a  className={style.socialIcon} href="#"></a>
                 </div>
-                <p className={style.copyright}>© 2021 Все права защищены</p>
+                <p className={style.copyright}>© 2021 All rights reserved</p>
 
             </div>
         </div>
